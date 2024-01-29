e347be2d2ed196aa0164112143bfcb26148a772b
ejaz4 <ejazali4@protonmail.ch>
2024-01-29 00:25:20 +0000

    ✨ Configuration for mobile clients

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,7 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
+import fs from "fs";
 
 export async function GET(request: NextRequest) {
-	return NextResponse.json({ message: "Hello World" }, { status: 200 });
+	const manifest = JSON.parse(
+		fs.readFileSync("./harmoni.configuration.json", "utf8")
+	);
+	return NextResponse.json(manifest, { status: 200 });
 }
 
 export async function POST(request: NextRequest) {
